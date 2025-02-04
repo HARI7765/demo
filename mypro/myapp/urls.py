6487874eddcd13d3b254ee6b74a2f290527fdab1
harikrishna0001 <nharikrishnanm@gmail.com>
2025-02-04 17:40:26 +0530

    updated

--- mypro/myapp/urls.py
+++ mypro/myapp/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name="index"),
+    path('main',views.main,name="main"),
+    path('first',views.first,name="first"),
+    path('second',views.second,name="second")
     
 ]
--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    if request.POST:
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        obj1=detail(name=name,email=email)
+        obj1.save()
+    return render(request,"index.html")
+def main(request):
+    obj=detail.objects.all()
+    return render(request,"main.html",{"det":obj})
+def first(request):
+    return render(request,"first.html")
+def second(request):
+    return render(request,"second.html")
